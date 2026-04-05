e43ea546b0e862d5937a21e9506853c17518aed7
Vaishnavi <sinhavaishnavi020@gmail.com>
2026-04-05 13:39:50 +0530

    feat:Add Signup page

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,6 +1,11 @@
 import streamlit as st
-from auth import login_page
+from auth import login_page,signup_page
 st.set_page_config(page_title="Job Tracker",layout="wide")
+
+signup_page()
+if st.session_state.get("signup_success"):
+    st.success("Signup Successfull!")
+    st.session_state["signup_success"]=False
 
 login_page()
 if st.session_state.get("login_success"):
--- streamlit_app/auth.py
+++ streamlit_app/auth.py
@@ -1,6 +1,25 @@
 import streamlit as st
 from api import login,signup
 
+def signup_page():
+    with st.form("Signup Page"):
+        st.title("Signup")
+
+        name=st.text_input("Username")
+        email=st.text_input("Email")
+        passwrod=st.text_input("Password",type="password")
+
+        if st.form_submit_button("Signup"):
+            res=signup({"name":name,"email":email,"password":passwrod})
+
+            if res.status_code==200:
+                token=res.json()["access_token"]
+                st.session_state["token"]=token
+                st.session_state["signup_success"]=True
+                st.rerun
+            else: 
+                st.error("Something went wrong! Signup failed")
+
 def login_page():
     with st.form("Login Page"):
 
